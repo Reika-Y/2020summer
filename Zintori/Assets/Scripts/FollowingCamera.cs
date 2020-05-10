78176fe72eca358b5de42c6d6c4bd3bc0d6e9d2d
1014 <kk.kk.kk>
2020-05-11 00:05:30 +0900

    カメラ作成

---
+++ Zintori/Assets/Scripts/FollowingCamera.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+
+public class FollowingCamera : MonoBehaviour
+{
+    // 追尾するターゲット
+    [SerializeField] private GameObject target;
+    [SerializeField] private Vector3 offset;
+
+    [SerializeField] private float distance = 4.0f;
+    [SerializeField] private float polarAngle = 45.0f;
+    [SerializeField] private float azimuthalAngle = 45.0f;
+
+    [SerializeField] private float minDistance = 1.0f;
+    [SerializeField] private float maxDistance = 7.0f;
+    [SerializeField] private float minPolarAngle = 5.0f;
+    [SerializeField] private float maxPolarAngle = 75.0f;
+    [SerializeField] private float mouseXSensitivity = 5.0f;
+    [SerializeField] private float mouseYSensitivity = 5.0f;
+    [SerializeField] private float scrollSensitivity = 5.0f;
+
+    private void LateUpdate()
+    {
+        if(Input.GetMouseButton(0))
+        {
+            UpdateAngle(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y"));
+        }
+
+        UpdateDistance(Input.GetAxis("Mouse ScrollWheel"));
+
+        var lookAtPos = target.transform.position + offset;
+        UpdatePosition(lookAtPos);
+        transform.LookAt(lookAtPos);
+    }
+
+    // 角度の更新
+    void UpdateAngle(float x,float y)
+    {
+        x = azimuthalAngle - x * mouseXSensitivity;
+        azimuthalAngle = Mathf.Repeat(x, 360);
+
+        y = polarAngle + y * mouseYSensitivity;
+        polarAngle = Mathf.Clamp(y, minPolarAngle, maxPolarAngle);
+    }
+
+    void UpdateDistance(float scroll)
+    {
+        scroll = distance - scroll * scrollSensitivity;
+        distance = Mathf.Clamp(scroll, minDistance, maxDistance);
+    }
+
+    void UpdatePosition(Vector3 lookAtPos)
+    {
+        var da = azimuthalAngle * Mathf.Deg2Rad;
+        var dp = polarAngle * Mathf.Deg2Rad;
+        transform.position = new Vector3(
+            lookAtPos.x + distance * Mathf.Sin(dp) * Mathf.Cos(da),
+            lookAtPos.y + distance * Mathf.Cos(dp),
+            lookAtPos.z + distance * Mathf.Sin(dp) * Mathf.Cos(da));
+    }
+}
