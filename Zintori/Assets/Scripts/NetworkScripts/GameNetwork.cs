d6b648304c69bd37ee439be83c91cf3cdd3b0af3
miyabi573 <cz1.7596hg[gmail.com>
2020-09-14 01:21:54 +0900

    セレクトシーン、ゲームシーン変更 セレクトシーンはなんか変えた、ゲームシーンはネットワーク用のオブジェクトを追加 プレイヤープレファブ更新 プレイヤープレファブにはPhotonViewスクリプトを追加 GameNetworkスクリプト更新 プレイヤーの生成位置の幅を調整

--- Zintori/Assets/Scripts/NetworkScripts/GameNetwork.cs
+++ Zintori/Assets/Scripts/NetworkScripts/GameNetwork.cs
@@ -13,11 +13,12 @@ public class GameNetwork : MonoBehaviour
     // 同期するプレイヤーオブジェクトの名前
     [SerializeField]
     private string InstansObjectName = "Player";
+    // インスタンスしたオブジェクト保持用
     private GameObject MyPlayerObject = null;
 
     void Start()
     {
         //　自身のプレイヤーを生成
-        MyPlayerObject = PhotonNetwork.Instantiate(InstansObjectName, new Vector3(Random.Range(-100, 100), 0, Random.Range(-100, 100)), Quaternion.identity);
+        MyPlayerObject = PhotonNetwork.Instantiate(InstansObjectName, new Vector3(Random.Range(-20, 20), 0, Random.Range(-20, 20)), Quaternion.identity);
     }
 }
